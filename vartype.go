65059484d6c3a83de4bb055f8fc9e27bc6d49f40
Dadaso Zanzane <dadaso@perpetualny.com>
2016-10-14 19:25:21 +0530

    day 1st programs

---
+++ vartype.go
@@ -0,0 +1,21 @@
+//define a package
+package main
+
+//import packages
+import "fmt"
+
+
+//The main function
+func main() {
+   // Declare a variable
+   var x float64
+
+   //Assign a value
+   x = 20.5
+
+   //Print the value
+   fmt.Println(x)
+
+   //print a type of varaible
+   fmt.Printf("x is of type %T\n", x)
+}
